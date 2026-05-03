35547ee31adf4417865321e992f2bdd3c2ab36f8
clara1110 <u11216011@go.utaipei.edu.tw>
2026-05-03 15:41:36 +0800

    remove practice files

--- app/main.py
+++ app/main.py
@@ -3,13 +3,11 @@ import math
 import httpx
 from fastapi import FastAPI, Query, HTTPException
 from dotenv import load_dotenv
-from app.api.items import router as items_router
 from app.api.test import router as test_router
 
 load_dotenv()
 
 app = FastAPI(title="淹水預警系統 API")
-app.include_router(items_router, prefix="/api")
 app.include_router(test_router, prefix="/api")
 
 
